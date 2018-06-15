1efad1fb8e5fddc20c77bb6328ad9d7ba4994e45
Rhiad Ciccoli <who.4004@gmail.com>
2018-06-15 02:38:49 -0300

    Update cgiC.py

--- cgiC.py
+++ cgiC.py
@@ -1,21 +1,16 @@
-#!C:\Users\lost\AppData\Local\Programs\Python\Python36-32\python.exe
 
-import cgi
 
+import requests, re, json, cgi
+from bs4 import BeautifulSoup as Soup
  
 print ("Content-Type: text/json\n\n")
 fields = cgi.FieldStorage()
-palavra = fields.getvalue('palavra')
+word = fields.getvalue('palavra')
 url = fields.getvalue('url')
-
-
-
-import requests, re, json
-from bs4 import BeautifulSoup as Soup
-r=requests.get(url)
-c=r.content
-a=Soup(c,"html.parser")
-fs = a.find_all(text =re.compile(palavra))
+totalRequests=requests.get(url)
+totalContent=totalRequests.content
+parsedContent=Soup(c,"html.parser")
+fs = parsedContent.find_all(text =re.compile(palavra))
 data = {'total_string' : len(fs)}
 json_str = json.dumps(data)
 return json_str
